d999222eec47548f6ddb56a3a425f8500916ceba
zormad9 <akmroz@zoho.com>
2026-08-03 16:24:42 +0200

    Update MainApp.java

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
 public class MainApp {
-
-  // testing testing 123
-
+    public static void main(String[] args) {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+        
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
